dc991e68a8b5f2b68151fc4062b8fa2937f5a936
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2024-01-22 14:39:06 +0100

    feat: implement basic version of answer widget

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateFeedback = event => {
+    const buttonText = event.target.innerHTML;
+
+    this.setState({
+      [buttonText.toLowerCase()]: this.state[buttonText.toLowerCase()] + 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Please leave a feedback</p>
+        <button onClick={this.updateFeedback}>Good</button>
+        <button onClick={this.updateFeedback}>Neutral</button>
+        <button onClick={this.updateFeedback}>Bad</button>
+        <p>Statistics</p>
+        <ul>
+          <li>
+            <p>Good: {this.state.good}</p>
+          </li>
+          <li>
+            <p>Neutral: {this.state.neutral}</p>
+          </li>
+          <li>
+            <p>Bad {this.state.bad}</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
